c8cba1e86076e30b330b676f1da5854494d57303
foxxalpha <64504988+foxxalpha@users.noreply.github.com>
2020-08-30 18:37:49 -0400

    completed done with help

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const whatTime = new Date();
+const currentTime = whatTime.getHours();
 
-console.log(whatTime);
+console.log(currentTime);
+
+let greeting;
+const customStyle = {
+  color: ""
+};
+
+if (currentTime < 12) {
+  greeting = "Good Morning";
+  customStyle.color = "red";
+} else if (currentTime < 18) {
+  greeting = "Good Afternoon";
+  customStyle.color = "green";
+} else {
+  greeting = "Good Night";
+  customStyle.color = "blue";
+}
 
 ReactDOM.render(
-  <h1 className="heading" style={}>
-    Good Afternoon
+  <h1 className="heading" style={customStyle}>
+    {greeting}
   </h1>,
   document.getElementById("root")
 );
